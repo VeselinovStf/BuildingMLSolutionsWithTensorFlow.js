76b806b412a7851e08c44d62bf6049c68f6e17b1
chofexx <chofexx@gmail.com>
2023-07-22 07:09:46 +0300

    Adding chapter 2 exercise

--- 02_SettingUpEnvironment/packageManager/src/client.js
+++ 02_SettingUpEnvironment/packageManager/src/client.js
@@ -1,4 +1,20 @@
 import * as tf from '@tensorflow/tfjs'
 
+/*
+
+import '@tensorflow/tfjs-backend-wasm'
+// Don't forget to add wasm static files
+
+*/
+
 console.log(tf.version);
-console.log(tf.version.tfjs)
+console.log(tf.version.tfjs);
+
+/* TF Backend */
+console.log(tf.getBackend());
+
+tf.setBackend('cpu');
+
+tf.ready().then(() => {
+    console.log(tf.getBackend())
+})
