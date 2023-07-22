39c3a0def5ea00da287abec8783efb83f66d1a38
chofexx <chofexx@gmail.com>
2023-07-22 07:59:13 +0300

    Compleating part 4

--- 03_UnderstandingTFCoreConcepts/src/client.js
+++ 03_UnderstandingTFCoreConcepts/src/client.js
@@ -1,20 +1,44 @@
 import * as tf from '@tensorflow/tfjs'
 
-const age = tf.tensor1d([20, 34], 'int32');
-age.print();
-tf.print(age);
+// Memory Management
+console.log('Memory Management');
+tf.tidy(() => {
 
-console.log(age.shape);
-console.log(age.dtype);
+    // Tensors
+    console.log('TESNORS');
+    const age = tf.tensor1d([20, 34], 'int32');
+    age.print();
+    tf.print(age);
 
-const ageAndWeight = tf.tensor2d([[23, 43], [50, 68]]);
-ageAndWeight.print(ageAndWeight);
+    console.log(age.shape);
+    console.log(age.dtype);
 
-console.log(ageAndWeight.shape);
-console.log(ageAndWeight.dtype);
+    const ageAndWeight = tf.tensor2d([[23, 43], [50, 68]]);
+    ageAndWeight.print(ageAndWeight);
 
-const scalar = tf.scalar(22);
-scalar.print(scalar);
+    console.log(ageAndWeight.shape);
+    console.log(ageAndWeight.dtype);
 
-console.log(scalar.shape);
-console.log(scalar.dtype);
+    const scalar = tf.scalar(22);
+    scalar.print(scalar);
+
+    console.log(scalar.shape);
+    console.log(scalar.dtype);
+
+    // Operations
+    console.log('Operations');
+
+    const income_one = tf.tensor1d([200, 300, 400]);
+    const income_two = tf.tensor1d([800, 700, 600]);
+
+    const total_income = tf.add(income_one, income_two);
+    total_income.print();
+
+    
+    console.log(tf.memory().numTensors);
+
+})
+
+console.log('How many tensors left after memory cleand?')
+console.log(tf.memory().numTensors);
+
