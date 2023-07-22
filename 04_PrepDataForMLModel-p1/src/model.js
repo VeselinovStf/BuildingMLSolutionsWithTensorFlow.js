cb436d70d9020609d8416963eb861b0eb5e027e8
chofexx <chofexx@gmail.com>
2023-07-22 10:53:30 +0300

    Adding Data visualization

--- 04_PrepDataForMLModel-p1/src/model.js
+++ 04_PrepDataForMLModel-p1/src/model.js
@@ -1,4 +1,5 @@
 import * as tf from '@tensorflow/tfjs'
+import * as tfvis from '@tensorflow/tfjs-vis'
 
 export const train = () => {
     tf.tidy(() => {
@@ -20,10 +21,34 @@ const readRawData = () => {
     return readData;
 }
 
+const plotOutputLabelOutput = (labels) => {
+    const labelsCount = labels.reduce((acc, label) => {
+        acc[label] = acc[label] === undefined ? 1 : acc[label] += 1;
+        return acc;
+    }, {});
+
+    const data = [];
+
+    Object.keys(labelsCount).forEach((key) => {
+        data.push({
+            index: key,
+            value: labelsCount[key]
+        })
+    });
+
+    tfvis.render.barchart({
+        tab: 'Explor',
+        name: 'Toxic Labels'
+    }, data,{});
+}
+
 const run = async () => {
     const rawDataResult = readRawData();
+    const labels = [];
 
     await rawDataResult.forEachAsync((r) => {
-        console.log(r);
+        labels.push(r['ys']['toxic']);
     })
+
+    plotOutputLabelOutput(labels);
 }
